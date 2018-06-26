63c6c3a8bc0899830d5f2265df82fd5a34914729
Kelyn Mann <kelyn.mann@irvmat.com>
2018-06-26 02:37:16 -0400

    Add navbar and some styling

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Provider} from 'react-redux';
+import {configureStore} from '../store';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Navbar from './Navbar';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div className="onboarding">
+        <Navbar />
       </div>
-    );
-  }
-}
+    </Router>
+  </Provider>
+);
 
 export default App;
