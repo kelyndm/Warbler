58f8e0de08e613e25e5b9f32242c0a4c44336bf7
Kelyn Mann <kelyn.mann@irvmat.com>
2018-06-29 01:33:44 -0400

    Add hoc to check for logged in user before allowing them to add a new message

---
+++ warbler-client/src/hocs/withAuth.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+export default function withAuth(CompoenentToBeRendered) {
+    class Authenticate extends Component {
+        componentWillMount() {
+            if(this.props.isAuthenticated === false) {
+                this.props.history.push("/signin");
+            }
+        }
+        componentWillUpdate(nextProps) {
+            if(nextProps.isAuthenticated === false) {
+                this.props.history.push("/signin");
+            }
+        }
+        render() {
+            return <CompoenentToBeRendered {...this.props} />
+        }
+    }
+    function mapStateToProps(state) {
+        return {
+            isAuthenticated: state.currentUser.isAuthenticated``
+        }
+    }
+    return connect(mapStateToProps)(Authenticate);
+}
+
+
+
